1778415dd40c77b4d22aed2db0cdd22c2668b760
YeisonOspitia <118226100+YeisonOspitia@users.noreply.github.com>
2023-02-15 15:36:53 -0500

    carpetas

---
+++ PLANTILLAS/registro_vacio.inc.php
@@ -0,0 +1,25 @@
+
+    <div class="form-group">
+        <label for="">Nombre de Usuario:</label>
+        <input type="text" class="form-control" name="nombre" placeholder="Ejem: Desarrolador_novato">
+    </div>
+    <div class="form-group">
+        <label for="">email:</label>
+        <input type="email" class="form-control" name="email" placeholder="Ejem: novato@hotmail.com">
+    </div>
+    <div class="form-group">
+        <label for="">Contraseña: </label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="form-group">
+        <label for="">Repite la contraseña:</label>
+        <input type="password" class="form-control" name="password2">
+    </div>
+    <br>
+    <button type="reset" class="btn btn-default btn-primary">Limpiar</button>
+    <br>
+    <br>
+    <button type="submit" class="btn btn-default btn-primary" name="enviar">Enviar</button>
+
+
+
